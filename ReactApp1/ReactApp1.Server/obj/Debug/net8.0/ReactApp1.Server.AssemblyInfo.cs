a7acad3a0c91990b1bdbf0c9e5435cb15cdec275
Albionaahmetti <82825446+Albionaahmetti@users.noreply.github.com>
2024-09-21 16:54:02 +0200

    Ishalla pot bon

--- ReactApp1/ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
+++ ReactApp1/ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f04689deac78095281abde0dc50901e7739a714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9056f7aaa892cbe51358020491e068f1ab0c6339")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
