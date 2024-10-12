06f64f4b8b6e02b67373ad83d51ed07653386ec7
Albionaahmetti <82825446+Albionaahmetti@users.noreply.github.com>
2024-10-12 15:48:04 +0200

    Added new dashboard link in index page

--- ReactApp1/reactapp1.client/src/App.tsx
+++ ReactApp1/reactapp1.client/src/App.tsx
@@ -1,32 +1,26 @@
-
-
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 import MainLayout from '../layouts/MainLayout';
 import NotFoundPage from '../pages/NotFound';
 import Products from './products/Products';
+import Home from './Home';
 import StockIN from './stockEntries/StockEntry';
-const router = createBrowserRouter(
-    createRoutesFromElements(<Route path='/' element={<MainLayout />} >
-        <Route path="/products" element={<Products />} />
-        <Route path="/stockentries" element={<StockIN />} />
-        <Route path='*' element={<NotFoundPage />} />
 
-    </Route>)
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<MainLayout />} >
+            <Route index element={<Home />} /> 
+            <Route path='/products' element={<Products />} />
+            <Route path='/stockentries' element={<StockIN />} />
+            <Route path='*' element={<NotFoundPage />} />
+        </Route>
+    )
 );
-function App()
-{
+
+function App() {
     return (
-        <RouterProvider router={router }></RouterProvider>
+        <RouterProvider router={router}></RouterProvider>
     );
 }
-//function App() {
-//    return (
-//        <Router>
-//            <Routes>
-//                <Route path="/" element={<Home />} />
-//            </Routes>
-//        </Router>
-//    );
-//}
+
 export default App;
