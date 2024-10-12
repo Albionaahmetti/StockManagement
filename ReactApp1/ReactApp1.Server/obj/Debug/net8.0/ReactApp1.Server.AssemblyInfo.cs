4961cbdef362dff88d7f5f0296363468fb6c3d6a
Albionaahmetti <82825446+Albionaahmetti@users.noreply.github.com>
2024-10-12 19:06:03 +0200

    aaa

--- ReactApp1/ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
+++ ReactApp1/ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3293a1738a980735cf739320634440b1ff21737e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e506bf15fda82be0797ca4aa828c60b7f5ac1ef9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
